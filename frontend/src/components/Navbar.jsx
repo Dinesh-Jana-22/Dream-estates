14c196487e07fab5070f8f962faf2040d72096ca
Dinesh Jana <dinesh.jana22xt@gmail.com>
2024-12-29 20:22:36 +0530

    Responsive layout for mobile add.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='bg-white text-black flex justify-around items-center py-2'>
-      <h1 className='font-fontInter text-3xl font-bold'>
-        Dream <span className='text-blue-400 font-extrabold'>Estates</span>
+    <div className='bg-white text-black flex justify-around items-center py-2 shadow-md'>
+      <h1 className='font-fontInter text-xl lg:text-3xl font-bold'>
+        Dream <span className='text-blue-500 font-extrabold'>Estates</span>
       </h1>
       <div className='flex items-center gap-8'>
-        <form className='bg-slate-200 flex items-center px-3 rounded-lg py-1'>
-            <input type="search" className='font-fontInter text-xl px-3 py-2 bg-transparent text-black focus:outline-none' placeholder='Search...'/>
-            <FaSearch className='text-gray-400 size-3 xl:size-6'></FaSearch>
+        <form className='bg-slate-100 lg:flex items-center px-3 rounded-lg py-1 hidden'>
+            <input type="search" className='font-fontInter text-lg px-3 py-2 bg-transparent text-black focus:outline-none' placeholder='Search...'/>
+            <FaSearch className='text-gray-300 size-4 xl:size-6'></FaSearch>
         </form>
         <ul className='flex gap-4  items-center'>
-            <li className='cursor-pointer hover:text-blue-400 transition-all'><a>Home</a></li>
-            <li className='cursor-pointer hover:text-blue-400 transition-all'><a>About</a></li>
-            <li className='cursor-pointer hover:text-blue-400 transition-all'><a>Contact</a></li>
+            <li className='cursor-pointer hover:text-blue-400 transition-all hidden md:block'><a>Home</a></li>
+            <li className='cursor-pointer hover:text-blue-400 transition-all hidden md:block'><a>About</a></li>
+            <li className='cursor-pointer hover:text-blue-400 transition-all hidden md:block'><a>Contact</a></li>
             <li><button className='bg-blue-600 px-4 py-2 rounded-lg font-fontInter text-white hover:bg-blue-700 transition-all'>Sign Up</button></li>
         </ul>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,14 +3,14 @@ import hero from '../assets/hero.jpg'
 
 const Home = () => {
   return (
-    <div className='flex justify-between items-center py-8 h-[90vh] gap-8'>
-      <div className='flex flex-col gap-4 justify-center pl-14'>
-        <h1 className='font-fontInter text-2xl lg:text-4xl xl:text-6xl font-bold'>Find your <span className='text-blue-500'>Dream</span> Property.</h1>
+    <div className='flex md:justify-between md:items-center py-8 md:h-[90vh] md:gap-8 gap-4 flex-col-reverse md:flex-row'>
+      <div className='flex flex-col gap-4 justify-center md:pl-14 px-6'>
+        <h1 className='font-fontInter text-xl md:text-2xl lg:text-4xl xl:text-6xl font-bold'>Find your <span className='text-blue-500'>Dream</span> Property.</h1>
         <p className='font-fontInter text-xl font-normal'>We have houses, apartments, and commercial spaces that fits your needs.</p>
-        <button className='bg-blue-500 px-10 py-4 rounded-lg font-normal text-2xl text-white w-fit hover:bg-blue-600 transition-all mt-5'>Search Now</button>
+        <button className='bg-blue-500 px-4 py-2 lg:px-10 lg:py-4 rounded-lg font-normal text-lg lg:text-2xl text-white w-fit hover:bg-blue-600 transition-all mt-5'>Search Now</button>
       </div>
-      <div className='w-1/2 max-w-[900px] flex items-end'>
-        <img src={hero} alt="" className='rounded-tl-[80px]'/>
+      <div className='md:w-1/2 md:min-w-[500px] flex items-end w-full'>
+        <img src={hero} alt="" className='md:rounded-tl-[80px]'/>
       </div>
     </div>
   )
